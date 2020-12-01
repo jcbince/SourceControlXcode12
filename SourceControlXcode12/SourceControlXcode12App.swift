b895fb2273988aaf01b13f24a3a4b009842664bd
Todd Perkins <todd@toddperkins.com>
2020-11-30 21:40:37 -0700

    Initial Commit

---
+++ SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by Todd Perkins on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
